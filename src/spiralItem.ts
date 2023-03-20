a52cb46ce7a872840949f4178bb256c05bcfc90b
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-20 10:10:40 +0900

    線形補間の式を定義

--- src/spiralItem.ts
+++ src/spiralItem.ts
@@ -68,3 +68,8 @@ export default class SpiralItem {
         parent.add(this.object)
     }
 }
+
+
+function lerp(x0: number, y0: number, x1: number, y1: number, x: number) {
+    return y0 + (x - x0) * (y1 - y0) / (x1 - x0)
+}
