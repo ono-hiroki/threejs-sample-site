3ca3f1778240afbeb8f019d21bf190ce54c9d405
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-20 10:05:25 +0900

    螺旋状にオブジェクトを配置

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
+import { MODELS } from "./define";
+import { load } from "./meshLoader";
 import renderingSystem from "./renderingSystem";
-import {MODELS} from "./define";
-import {load} from "./meshLoader";
 import spiralSystem from "./spriralSystem";
+
 
 Promise
     .all(MODELS.map(v => load(v)))
     .then(() => {
         spiralSystem.init()
         const loop = () => {
+            spiralSystem.exec()
             renderingSystem.exec()
             requestAnimationFrame(loop)
         }
         loop()
     })
-
 
